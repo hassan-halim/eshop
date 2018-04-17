2810efa2fdeecd016348b803af931d0ca62a6494
hassan-halim <hasan.ebrahim@hotmail.com>
2018-04-17 23:35:58 +0200

    Add files via upload

---
+++ Api/EshopApi/SharedKernel/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SharedKernel
+{
+    public abstract class Entity<TId> : IEquatable<Entity<TId>>
+    {
+        public TId Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastChanged { get; set; }
+
+        public virtual bool Equals(Entity<TId> otherObject)
+        {
+            var entity = otherObject as Entity<TId>;
+            if (entity != null)
+            {
+                return this.Equals(entity);
+            }
+            return base.Equals(otherObject);
+        }
+        public override int GetHashCode()
+        {
+            return this.Id.GetHashCode();
+        }
+    }
+}
---
+++ Api/EshopApi/SharedKernel/KeyValuePairResource.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedKernel
+{
+    public class KeyValuePairResource <Tid>
+    {
+        public Tid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
